544dc59b606dbb01e7f6c01348b5142b4b2467cd
Jimmy <jimmymei96@gmail.com>
2023-07-01 13:32:49 -0500

    practice question 1-3

---
+++ week3.js
@@ -0,0 +1,14 @@
+//Question 1
+const name = "Jimmy"
+console.log(name)
+
+//Question 2
+let length = 10
+console.log(length + 15)
+
+//Question 3
+const studentId = '1234'
+const studentName = "Jimmy"
+const schoolName = "Chicago School"
+console.log("My name is " + studentName + ". My student ID is " + studentId + " and I go to " + schoolName + ".")
+
