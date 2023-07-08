01aabb13e632dd89cb2ec8557d254232f1e55e07
Jimmy <jimmymei96@gmail.com>
2023-07-08 13:39:06 -0500

    password checker

---
+++ boolean2.js
@@ -0,0 +1,16 @@
+//console.log(true && true);
+//console.log(true && false);
+//console.log(false && false);
+
+//console.log(true || true);
+//console.log(true || false);
+//console.log(false || false);
+
+function strongPassword(password) {
+  if (password === "strong-password") {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(strongPassword("strong-password"));
