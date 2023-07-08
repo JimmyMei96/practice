e64c04c0ad50e683cd7deaa7049ca8dd5f857ff3
Jimmy <jimmymei96@gmail.com>
2023-07-08 14:49:59 -0500

    add forLoop

--- boolean2.js
+++ boolean2.js
@@ -6,6 +6,8 @@
 //console.log(true || false);
 //console.log(false || false);
 
+// PASSWORD CHECKER
+
 function strongPassword(password) {
   if (password === "strong-password") {
     return true;
@@ -13,4 +15,25 @@ function strongPassword(password) {
     return false;
   }
 }
-console.log(strongPassword("strong-password"));
+// console.log(strongPassword("strong-password"));
+
+// NUMBER CHECKER
+
+function numberChecker(number1, number2) {
+  if (number1 > 0 && number2 > 0) {
+    return true;
+  } else return false;
+}
+//console.log(numberChecker(43, 6));
+
+function forLoop() {
+  for (let i = 0; i < 100; i = i + 1) {
+    // if (i % 2 === 0) {
+    //   console.log(i * i);
+    // }
+    if (i % 2 === 1) {
+      console.log("odd", i);
+    }
+  }
+}
+forLoop();
