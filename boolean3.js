15835ca6979d59eb65843e254659761a1c76ab32
Jimmy <jimmymei96@gmail.com>
2023-07-15 14:41:32 -0500

    add Arrays

--- boolean3.js
+++ boolean3.js
@@ -16,4 +16,25 @@ const colors = ["Red", "Blue", "Yellow", "Purple", "Green"];
 const sports = ["Basketball", "Soccor", "Football", "Baseball"];
 const temperatures = [85, 83, 91, 78, 21, 54];
 
-console.log(colors.length);
+function myFavoriteColor(colors) {
+  for (i = 0; i < colors.length; i = i + 1) {
+    console.log("My favorite color is " + colors[i]);
+  }
+}
+
+//myFavoriteColor(colors);
+
+function favoriteSport(sports) {
+  for (i = 0; i < sports.length; i = i + 1) {
+    console.log("My favorite sport to play is " + sports[i]);
+  }
+}
+//favoriteSport(sports);
+
+function currentTemp(temperatures) {
+  for (i = 0; i < temperatures.length; i = i + 1) {
+    console.log("It is " + temperatures[i] + " degrees today.");
+  }
+}
+
+currentTemp(temperatures);
