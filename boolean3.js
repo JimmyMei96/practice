d594a767be628952020a0cd238a6b02777d2942e
Jimmy <jimmymei96@gmail.com>
2023-07-15 13:28:09 -0500

    add array length

---
+++ boolean3.js
@@ -0,0 +1,19 @@
+function twentyNumbers() {
+  for (let i = 1; i < 21; i = i + 1) {
+    console.log(i);
+  }
+}
+// twentyNumbers();
+
+function oddNumbers() {
+  for (let i = 1; i < 100; i = i + 2) {
+    console.log(i);
+  }
+}
+//oddNumbers();
+
+const colors = ["Red", "Blue", "Yellow", "Purple", "Green"];
+const sports = ["Basketball", "Soccor", "Football", "Baseball"];
+const temperatures = [85, 83, 91, 78, 21, 54];
+
+console.log(colors.length);
